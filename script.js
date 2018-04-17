83115379a3cbcc4e59812d0646afcc5c6aefcae6
nehamj <nehamj798@gmail.com>
2018-04-17 13:32:25 +0530

    Bug fixing

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 $(document).ready(function(){
-
-    //waypoints
-    //down
     $('.wp-zone').waypoint(function(direction) {
     if (direction === 'down') {
       $('nav a').removeClass('disable-click');
-      // form the selector dynamically.
-      // "this" keyword refers waypoint object and the element is located at "this.element"
-      // using "this.element.id", get the nav anchor you want to target
-      // example: "nav a[href='#landing']"
       var selector = "nav a[href='#" + this.element.id + "']";
       $(selector).addClass('disable-click');
     }
     }, {
     offset: '5%'
   });
-  //up
+  
     $('.wp-zone').waypoint(function(direction) {
       if (direction === 'up') {
         $('nav a').removeClass('disable-click');
@@ -27,12 +20,21 @@ $(document).ready(function(){
   });
 
   $('#trigger').click( function(event) {
-    event.stopPropagation();
-    $('#navid').toggle();
-  });
+    if($(window).width() < 786){
+      event.stopPropagation();
+      $('#navid').toggle();
 
-  $(document).click( function() {
-        $('#navid').hide();
-  });
+  
+      $(document).click( function() {
+         $('#navid').hide();
+      });  
+    }  
+   });
+    
+$(window).resize(function (){
+  if($(window).width() > 786){
+    $('#navid').show();
+  }
+});
 
   });
